86fbcd28eae297999ba60b3d8ec3b5bf7ffb5e7b
Egor MacOS <zhelagin.egor@outlook.com>
2023-08-25 22:06:03 +0300

    Added reader for config file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"path"
+)
+
+type (
+	Config struct {
+		Storage `yaml:"storage"`
+	}
+
+	Storage struct {
+		MaxPoolSize int `env-required:"true" yaml:"max_pool_size"`
+	}
+)
+
+func MustLoadConfig(configPath string) (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig(path.Join("./", configPath), cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	err = cleanenv.UpdateEnv(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error updating env: %w", err)
+	}
+
+	return cfg, nil
+}
