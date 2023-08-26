c00300f67789fd02a12e1e6e3f2e5f7c28dcf2e7
Egor MacOS <zhelagin.egor@outlook.com>
2023-08-26 12:33:58 +0300

    Update config file and config.go

--- config/config.go
+++ config/config.go
@@ -6,28 +6,57 @@ import (
 	"path"
 )
 
+//PG_URL=postgres://{user}:{password}@{host}:{port}/{database}
+
 type (
 	Config struct {
+		App     `yaml:"app"`
+		HTTP    `yaml:"http"`
+		Log     `yaml:"log"`
 		Storage `yaml:"storage"`
 	}
 
+	HTTP struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	}
+
+	Log struct {
+		Level string `yaml:"level"`
+	}
+
+	App struct {
+		Name    string `yaml:"name"`
+		Version string `yaml:"version"`
+		Env     string `yaml:"env"`
+	}
+
 	Storage struct {
-		MaxPoolSize int `env-required:"true" yaml:"max_pool_size"`
+		MaxPoolSize int    `env-required:"true" yaml:"max_pool_size"`
+		Host        string `yaml:"host"`
+		Port        string `yaml:"port"`
+		User        string `yaml:"user"`
+		Password    string `yaml:"password"`
+		Database    string `yaml:"database"`
 	}
 )
 
-func MustLoadConfig(configPath string) (*Config, error) {
+func MustLoadConfig(configPath string) *Config {
 	cfg := &Config{}
 
 	err := cleanenv.ReadConfig(path.Join("./", configPath), cfg)
 	if err != nil {
-		return nil, fmt.Errorf("error reading config file: %w", err)
+		panic(err)
 	}
 
 	err = cleanenv.UpdateEnv(cfg)
 	if err != nil {
-		return nil, fmt.Errorf("error updating env: %w", err)
+		panic(err)
 	}
 
-	return cfg, nil
+	return cfg
+}
+
+func PgUrl(user, password, host, port, database string) string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, database)
 }
