3f06516d77546af42f3a6eff4865f65aba6efade
Egor MacOS <zhelagin.egor@outlook.com>
2023-08-26 10:52:39 +0300

    Added a simple swag api for main function (test)

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// @title Dynamic User Segmentation Swagger API
+// @version 0.0.1
+// @description
+// @termOfService http://swagger.io/terms/
+
+// @contact.name Egor Zhelagin
+// @contact.email zhelagin.egor@yandex.ru
+
+// @license.name MIT
+// @license.url
 
 func main() {
 	fmt.Println("Hello world")
