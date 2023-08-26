e0f4683f8876a9c0ecfc4c16660578e9b11dc3d5
Egor MacOS <zhelagin.egor@outlook.com>
2023-08-27 00:03:27 +0300

    Some fix main.go and docker-compose

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"context"
 	"dynamic-user-segmentation/config"
-	"dynamic-user-segmentation/pkg/client/db"
+	"dynamic-user-segmentation/pkg/client/db/postgresql"
 	"dynamic-user-segmentation/pkg/logger/sloglogger"
-	"dynamic-user-segmentation/pkg/util/erros"
+	"dynamic-user-segmentation/pkg/util/errors"
 	"os"
 )
 
@@ -32,18 +33,29 @@ func main() {
 	logger := sloglogger.NewLogger(sloglogger.SetLevel(cfg.Log.Level))
 
 	logger.Info("Initializing client postgreSQl")
-	_, err := db.NewClient(config.PgUrl(
+	pg, err := postgresql.NewClient(config.PgUrl(
 		cfg.Storage.User,
 		cfg.Storage.Password,
 		cfg.Storage.Host,
 		cfg.Storage.Port,
-		cfg.Storage.Database), db.LoadMaxPoolSize(cfg.Storage.MaxPoolSize))
+		cfg.Storage.Database), postgresql.LoadMaxPoolSize(cfg.Storage.MaxPoolSize))
 
 	if err != nil {
-		logger.Error("App run  error: %w", erros.Wrap("can't crete client", err))
+		logger.Error("App run  error: %w", errors.Wrap("can't crete client", err))
 		os.Exit(1)
 	}
 
 	logger.Info("Initializing repositories")
 
+	_, err = pg.Pool.Exec(context.Background(), "INSERT INTO test(name) VALUES ($1)", "")
+	if err != nil {
+		logger.Error(err.Error())
+	}
+
+	var name string
+	err = pg.Pool.QueryRow(context.Background(), "SELECT name FROM test WHERE ID = $1", 3).Scan(&name)
+	if err != nil {
+		logger.Error(err.Error())
+	}
+	logger.Info("Name from table: ", name)
 }
