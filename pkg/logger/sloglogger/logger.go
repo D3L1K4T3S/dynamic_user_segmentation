fb639285b0929887c11fcc8058d5815b81817594
Egor MacOS <zhelagin.egor@outlook.com>
2023-08-26 12:36:03 +0300

    Update logger, then can set logger level from config file

--- pkg/logger/sloglogger/logger.go
+++ pkg/logger/sloglogger/logger.go
@@ -16,3 +16,14 @@ func NewLogger(level slog.Level) *slog.Logger {
 
 	return slog.New(options.NewSlogHandler(os.Stdout))
 }
+
+func SetLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	default:
+		return slog.LevelDebug
+	}
+}
