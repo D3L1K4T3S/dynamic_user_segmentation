c1fc97ab8fa592636c2aba3715ffdaa0e32c0580
Egor MacOS <zhelagin.egor@outlook.com>
2023-08-28 13:37:29 +0300

    Добавлен сервис для 'actions'

--- internal/service/actions.go
+++ internal/service/actions.go
@@ -2,8 +2,9 @@ package service
 
 import (
 	"context"
-	"dynamic-user-segmentation/internal/entity"
 	"dynamic-user-segmentation/internal/repository"
+	"dynamic-user-segmentation/internal/service/dbo"
+	e "dynamic-user-segmentation/pkg/util/errors"
 )
 
 type ActionsService struct {
@@ -14,9 +15,23 @@ func NewActionsService(actionsRepository repository.Actions) *ActionsService {
 	return &ActionsService{actions: actionsRepository}
 }
 
-func (as *ActionsService) GetActionById(ctx context.Context, id int) (entity.Action, error) {
-	return as.actions.GetActionById(ctx, id)
+func (as *ActionsService) CreateAction(ctx context.Context, action dbo.ActionsRequest) (int, error) {
+	return as.actions.CreateAction(ctx, action.Name)
 }
-func (as *ActionsService) GetIdByAction(ctx context.Context, action entity.Action) (int, error) {
-	return as.actions.GetIdByAction(ctx, action)
+func (as *ActionsService) DeleteAction(ctx context.Context, action dbo.ActionsRequest) error {
+	return as.actions.DeleteAction(ctx, action.Name)
+}
+func (as *ActionsService) GetActionById(ctx context.Context, id int) (dbo.ActionsResponse, error) {
+	var actions dbo.ActionsResponse
+	res, err := as.actions.GetActionById(ctx, id)
+	if err != nil {
+		return dbo.ActionsResponse{}, e.Wrap("can't do service: ", err)
+	}
+	actions.Id = res.Id
+	actions.Name = res.Name
+
+	return actions, nil
+}
+func (as *ActionsService) GetIdByAction(ctx context.Context, action dbo.ActionsRequest) (int, error) {
+	return as.actions.GetIdByAction(ctx, action.Name)
 }
